0e4ed3837290682c431dce117d188aa53ab350a7
Mateusz Janicki <mj4nicki@gmail.com>
2026-06-03 14:46:14 +0200

    feat: add DashboardController with stats and active missions

---
+++ src/Controllers/DashboardController.php
@@ -0,0 +1,48 @@
+<?php
+
+class DashboardController extends AppController
+{
+    private MissionRepository   $missionRepo;
+    private EquipmentRepository $equipmentRepo;
+    private UserRepository      $userRepo;
+
+    public function __construct()
+    {
+        $this->missionRepo   = new MissionRepository();
+        $this->equipmentRepo = new EquipmentRepository();
+        $this->userRepo      = new UserRepository();
+    }
+
+    public function index(): void
+    {
+        SessionService::requireLogin();
+
+        $missionStats   = $this->missionRepo->getStats();
+        $equipmentStats = $this->equipmentRepo->getStats();
+        $activeMissions = $this->missionRepo->getActiveMissions();
+
+        // Aktywni ratownicy (przypisani do aktywnych akcji)
+        $activeRescuersCount = 0;
+        foreach ($activeMissions as $mission) {
+            $rescuers = $this->missionRepo->getMissionRescuers($mission->getId());
+            $activeRescuersCount += count($rescuers);
+        }
+
+        $this->render('dashboard/index', [
+            'missionStats'        => $missionStats,
+            'equipmentStats'      => $equipmentStats,
+            'activeMissions'      => $activeMissions,
+            'activeRescuersCount' => $activeRescuersCount,
+        ]);
+    }
+
+    public function apiStats(): void
+    {
+        SessionService::requireLogin();
+
+        $this->json([
+            'missions'  => $this->missionRepo->getStats(),
+            'equipment' => $this->equipmentRepo->getStats(),
+        ]);
+    }
+}
